3d0adeda9fc02309abfffc0dca6496ca7ad70316
Ioannis Jone Magalhaes <ij21317@essex.ac.uk>
2024-08-01 12:38:47 +0100

    Uses version 2024.1.4 of java now Yellow -> R : 251 G: 236 B:80 Cell render added to task list button panel added Add task button added(To Do: add functionality) Viewing tasks now shows all task details

--- src/main/java/com/example/todo/ToDoGUI.java
+++ src/main/java/com/example/todo/ToDoGUI.java
@@ -3,20 +3,30 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 public class ToDoGUI {
     JFrame frame;
     JPanel tasksPanel;
     JPanel descPanel;
+    JPanel buttonsPanel;
     JList<Task> tasks;
+    HashMap<String,String> colors = new HashMap<>();
+
     public ToDoGUI(TaskMaster master){
         frame = new JFrame();
         tasksPanel = new JPanel();
         descPanel = new JPanel();
-        tasksPanel.setBackground(Color.YELLOW);
-        descPanel.setBackground(Color.YELLOW);
+        buttonsPanel = new JPanel();
+        tasksPanel.setBackground(new Color(251, 236, 80));
+        descPanel.setBackground(new Color(251, 236, 80));
+        buttonsPanel.setBackground(Color.DARK_GRAY);
         tasksPanel.setPreferredSize(new Dimension(100,75));
         descPanel.setPreferredSize(new Dimension(100,100));
+        buttonsPanel.setPreferredSize(new Dimension(100,100));
+        JButton addTask = new JButton("Add Task");
         JLabel label = new JLabel();
         label.setPreferredSize(new Dimension(50,50));
         tasks = taskList(master,label);
@@ -25,16 +35,17 @@ public class ToDoGUI {
         tasksPanel.setLayout(new BorderLayout());
         descPanel.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setSize(800,800);
         frame.setTitle("To-Do");
-        frame.setVisible(true);
         tasks.setBackground(Color.YELLOW);
         tasks.setFont(new Font("Helvetica",Font.BOLD,15));
         tasksPanel.add(new JScrollPane(tasks),BorderLayout.CENTER);
         descPanel.add(label);
+        buttonsPanel.add(addTask);
         frame.add(tasksPanel,BorderLayout.NORTH);
-        frame.add(descPanel,BorderLayout.SOUTH);
-
+        frame.add(descPanel,BorderLayout.CENTER);
+        frame.add(buttonsPanel,BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
 
     public JList<Task> taskList(TaskMaster master,JLabel label){
@@ -56,12 +67,27 @@ public class ToDoGUI {
             viewTask(taskChosen,label);
         });
 
+        tasks.setCellRenderer((list, value, index, isSelected, cellHasFocus) -> {
+            JLabel selectedLabel = new JLabel(value.getTaskName());
+            if (isSelected) {
+                selectedLabel.setBackground(new Color(251, 236, 80));
+                selectedLabel.setForeground(Color.DARK_GRAY);
+            } else {
+                selectedLabel.setBackground(Color.DARK_GRAY);
+                selectedLabel.setForeground(new Color(251, 236, 80));
+            }
+            selectedLabel.setOpaque(true);
+            return selectedLabel;
+
+        });
+
     return tasks;
     }
 
+
     public void viewTask(Task task,JLabel label){
-        label.setText(task.getTaskDescription());
-        System.out.println("added desc");
+        label.setText(task.getTaskName() + " " + task.getTaskDescription() + " " + ((task.getCompleted())? "Complete" : "incomplete") + " " + task.getPriority());
+
     }
 
 }
